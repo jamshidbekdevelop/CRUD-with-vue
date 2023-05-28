9e78b67dba48048afe26741d0220116f1ab24886
jamshidbekdevelop <sharipovjamshidbek51@gmail.com>
2023-05-28 19:01:21 +0100

    start

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <h1>Hello Toshkent {{ counter }}</h1>
+    <button @click="onIncreament" class="btn btn-success">add</button>
+    <button @click="onDecreament " class="btn btn-danger">delete</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 1,
+    };
+  },
+  methods: {
+    onIncreament() {
+      this.counter += 1;
+    },
+    onDecreament() {
+      this.counter -= 1;
+    },
+  },
+};
+</script>
